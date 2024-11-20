2960dcd2e16b259021b6c4e501715f6442f1cf9c
AndreaMargutti <marguttiandrea30@gmail.com>
2024-11-20 16:27:00 +0100

    add more style

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -1,11 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="card">
-    <div class="card-body">
-        <h1 class="card-title">{{$post->title}}</h1>
-        <h2 class="card-title">{{$post->author}}</h2>
-        <p class="card-text">{{$post->content}}</p>
+<section class="container">
+    <div class="card text-center text-bg-info">
+        <div class="card-body">
+            <h1 class="card-title">{{$post->title}}</h1>
+            <h2 class="card-title">{{$post->author}}</h2>
+            <p class="card-text">{{$post->content}}</p>
+        </div>
     </div>
-</div>
+</section>
 @endsection

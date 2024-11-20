a13118d77c16589e4c566cea2149fa72f6270f05
AndreaMargutti <marguttiandrea30@gmail.com>
2024-11-20 15:27:35 +0100

    removed request

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Post;
-use App\Http\Request\UpdatePostsRequest;
 
 class PostController extends Controller
 {
@@ -32,20 +31,8 @@ class PostController extends Controller
 
     }
 
-    public function update (UpdatePostsRequest $request, string $id) {
+    public function update () {
 
-        $request->validated();
-
-        $newData = $request->all();
-
-        $post = Post::findOrFail($id);
-        $post->title = $newData["title"];
-        $post->author = $newData["author"];
-        $post->description = $newData["description"];
-
-        $post->save();
-
-        return redirect()->route("admin.posts.show", [ "id"=> $post->id] );
     }
 
     public function destroy () {

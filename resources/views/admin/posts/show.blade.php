bf07694e326b3c99b6d0f145bcf430735fe8e009
AndreaMargutti <marguttiandrea30@gmail.com>
2024-11-20 16:06:13 +0100

    add show for posts

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -1,12 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>{{$post->title}}</h1>
-</body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+<div class="card">
+    <div class="card-body">
+        <h1 class="card-title">{{$post->title}}</h1>
+        <h2 class="card-title">{{$post->author}}</h2>
+        <p class="card-text">{{$post->content}}</p>
+    </div>
+</div>
+@endsection
